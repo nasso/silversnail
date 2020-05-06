5670425e366c751b0a650ca04867e0c02532a64d
nasso <nassomails@gmail.com>
2020-05-06 05:34:02 +0200

    Rename host directory to host-native

--- build_plugins.sh
+++ build_plugins.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 PLUGINS_SRC="$(realpath ./plugins/*)"
-PLUGIN_DIR="$(realpath ./host/plugins)"
+PLUGIN_DIR="$(realpath ./host-native/plugins)"
 
-mkdir $PLUGIN_DIR
+mkdir -p $PLUGIN_DIR
 
 function build_plug() {
-    cd $1
-    cargo build --release
-    cp ./target/wasm32-unknown-unknown/release/*.wasm $PLUGIN_DIR
+  cd $1
+  cargo build --release
+  cp ./target/wasm32-unknown-unknown/release/*.wasm $PLUGIN_DIR
 }
 
 for plug in $PLUGINS_SRC; do
-    build_plug "$plug"
+  build_plug "$plug"
 done
 
 for plug in $PLUGIN_DIR/*.wasm; do
-    wasm-gc $plug
+  wasm-gc $plug
 done
